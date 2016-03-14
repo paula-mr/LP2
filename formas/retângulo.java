79728acd9a4aba940115a22b4b4ee5005575c137
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-03-13 23:38:17 -0300

    Update retângulo.java
    gambiarraça em double getlado1 e double getlado2

--- formas/retângulo.java
+++ formas/retângulo.java
@@ -7,8 +7,55 @@ package formas;
 
 /**
  *
- * @author CristinaMaradaSilvaR
+ * @author Paula Ribeiro
  */
 public class retângulo {
-    
+    private segmento lado1;
+    private segmento lado2;
+    public retângulo() {
+        lado1 = new segmento();
+        lado2 = new segmento();
+    }
+    public retângulo(segmento lado1, segmento lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    public retângulo (double l1, double l2) {
+        lado1 = new segmento();
+        lado2 = new segmento();
+        lado1.setSeg(l1);
+        lado2.setSeg(l2);
+    }
+    public void setLado1(segmento lado1) {
+        this.lado1 = lado1;
+    }
+    public void setLado1 (double l1) {
+        lado1.setSeg(l1);
+    }
+    public segmento getLado1 () {
+        return lado1;
+    }
+    public double getlado1 () {
+        double l1=lado1.getSeg();
+        return l1;
+    }
+    public double getlado2 () {
+        double l2=lado2.getSeg();
+        return l2;
+    }
+    public void setLado2(segmento lado2) {
+        this.lado2 = lado2;
+    }
+    public void setLado2 (double l2) {
+        lado2.setSeg(l2);
+    }
+    public segmento getLado2 () {
+        return lado2;
+    }
+    public double area () {
+        return lado1.getSeg()*lado2.getSeg();
+    }
+    public double perimetro () {
+        return 2*lado1.getSeg()+2*lado2.getSeg();
+    }
 }
