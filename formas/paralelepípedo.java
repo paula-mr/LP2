317aa769955247b62d53b0db154ff0feebbe1c46
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-03-13 23:34:09 -0300

    Update paralelepípedo.java
    gambiarra pra calcular area externa

--- formas/paralelepípedo.java
+++ formas/paralelepípedo.java
@@ -7,8 +7,43 @@ package formas;
 
 /**
  *
- * @author CristinaMaradaSilvaR
+ * @author Paula Ribeiro
  */
 public class paralelepípedo {
-    
+    private retângulo ret;
+    private segmento profund;
+    private segmento l1;
+    private segmento l2;
+    public paralelepípedo () {
+        ret = new retângulo();
+        profund = new segmento();
+    }
+    public paralelepípedo (retângulo ret, segmento profund) {
+        setRet(ret);
+        this.profund = profund;
+    }
+    public void setRet (retângulo ret) {
+        this.ret = ret;
+    }
+    public retângulo getRet() {
+        return ret;
+    }
+    public segmento getProfund() {
+        return profund;
+    }
+    public void setProfund(segmento profund) {
+        this.profund = profund;
+    }
+    public double areaexterna() {
+        double a = ret.area();
+        double p = profund.getSeg();
+        double alt = ret.getlado2();
+        double areae = 4*a + 2*p*alt;
+        return areae;
+    }
+    public double volume() {
+        double a = ret.area();
+        double p = profund.getSeg();
+        return a*p;
+    }
 }
