fcaac778778b5ddb47a868b6632268672e4852a2
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-03-14 09:22:18 -0300

    Update Aula4.java

--- aula4/Aula4.java
+++ aula4/Aula4.java
@@ -11,26 +11,25 @@ import javax.swing.JOptionPane;
  * @author Paula
  */
 public class Aula4 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Inicializando os objetos com construtores vazios
         pethouse CaeseGatos = new pethouse();
         cachorro Cachorro1 = new cachorro();
         gato Gato1 = new gato();
         // Atribuindo valor às variáveis de instância do Cachorro
-        Cachorro1.setSom("Au");
-        Cachorro1.setQuantalimentod(Integer.parseInt(JOptionPane.showInputDialog("Quantidade de alimento consumida pelo cachorro diariamente: ")));
+        Cachorro1.setSom("AuAu");
+        Cachorro1.setQuantalimentod(Integer.parseInt(JOptionPane.showInputDialog("Quantidade de alimento consumida pelo cachorro diariamente:")));
+        CaeseGatos.setTempoEstadiaCao(Integer.parseInt(JOptionPane.showInputDialog("Tempo de estadia do cachorro:")));
         // Atribuindo valor às variáveis de instância do Gato
         Gato1.setSom("Miau");
-        Gato1.setQuantalimentod(Integer.parseInt(JOptionPane.showInputDialog("Quantidade de alimento consumida pelo gato diariamente: ")));
-        //Atribuindo valor às variavéis da pethouse
+        Gato1.setQuantalimentod(Integer.parseInt(JOptionPane.showInputDialog("Quantidade de alimento consumida pelo gato diariamente:")));
+        CaeseGatos.setTempoEstadiaGato(Integer.parseInt(JOptionPane.showInputDialog("Tempo de estadia do gato:")));
+        // Atribuindo valor às variavéis da pethouse
         CaeseGatos.setNomedaloja("CaeseGatos");
         CaeseGatos.setCachorro(Cachorro1);
         CaeseGatos.setGato(Gato1);
-        CaeseGatos.setTempodeestadia(Integer.parseInt(JOptionPane.showInputDialog("Tempo de estadia dos animais:")));
+        // Retorna a quantidade total de alimento consumido
+        JOptionPane.showMessageDialog(null, "Quantidade de alimento total consumida pelo cachorro: " + CaeseGatos.TotalAlimento(CaeseGatos.getTempoEstadiaCao(), Cachorro1.getQuantalimentod()));
+        JOptionPane.showMessageDialog(null, "Quantidade de alimento total consumida pelo gato: " + CaeseGatos.TotalAlimento(CaeseGatos.getTempoEstadiaGato(), Gato1.getQuantalimentod()));
     }
-    
 }
