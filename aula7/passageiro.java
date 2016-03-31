55a8ec9d6d7407789aa18f2ef5906652494efb81
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-03-31 19:55:20 -0300

    Added files via upload
    https://drive.google.com/open?id=0B8jrKlCo-wv4TGNSWWZaVWJjLTJuTktxTE5NaVNfSHEzRUNB

---
+++ aula7/passageiro.java
@@ -0,0 +1,22 @@
+package aula7;
+
+/**
+ *
+ * @author Paula Ribeiro
+ */
+public class passageiro {
+    int id;
+    int voo; //variável para o voo desejado
+    void setId(int id) {
+        this.id=id;
+    }
+    int getId() {
+        return id;
+    }
+    void setVoo(int voo) {
+        this.voo=voo;
+    }
+    int getVoo() {
+        return voo;
+    }
+}
---
+++ aula7/vôo.java
@@ -0,0 +1,82 @@
+package aula7;
+
+/**
+ *
+ * @author Paula Ribeiro
+ */
+public class vôo {
+    int numVoo;
+    int tipoAvião;
+    int preço;
+    int capacidade;
+    int reservas;
+    passageiro passageiro[];
+    public vôo(){
+        passageiro = new passageiro[capacidade];
+        reservas = 0;
+    }
+    public vôo(int numVoo, int tipoAvião, int preço) {
+        this.numVoo = numVoo;
+        this.tipoAvião = tipoAvião;
+        this.preço = preço;
+        setCapacidade();
+        passageiro = new passageiro[capacidade];
+        reservas = 0;
+    }
+    public int getNumVoo() {
+        return numVoo;
+    }
+    public void setNumVoo(int numVoo) {
+        this.numVoo = numVoo;
+    }
+    public int getTipoAvião() {
+        return tipoAvião;
+    }
+    public void setTipoAvião(int tipoAvião) {
+        this.tipoAvião = tipoAvião;
+        setCapacidade();
+    }
+    public int getPreço() {
+        return preço;
+    }
+    public void setPreço(int preço) {
+        this.preço = preço;
+    }
+    public passageiro getPassageiro() {
+        return passageiro[reservas];
+    }
+    public void setPassageiro(passageiro p) {
+        passageiro[reservas] = new passageiro();
+        passageiro[reservas] = p;
+    }
+    public int getReservas() {
+        return reservas;
+    }
+    public void setReservas() {
+        reservas = 0;
+    }
+    public int getCapacidade() {
+        return capacidade;
+    }
+    public void setCapacidade() {
+        if(getTipoAvião()==707) {
+            capacidade=10;
+        }
+        else {
+            if(getTipoAvião()==727) {
+                capacidade=7;
+            }
+            else {
+                if(getTipoAvião()==737) {
+                    capacidade=2; //mudar pra 5
+                }
+            }
+        }
+    }
+    public boolean checkCapacidade() {
+        return reservas<capacidade;
+    }
+    public void reservas() {
+        reservas++;
+    }
+}
