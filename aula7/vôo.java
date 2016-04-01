cf8aa80e918a62527d9a13f930c14ff16fad767e
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-03-31 22:39:57 -0300

    Update vôo.java

--- aula7/vôo.java
+++ aula7/vôo.java
@@ -1,82 +1,41 @@
 package aula7;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Paula Ribeiro
  */
-public class vôo {
-    int numVoo;
-    int tipoAvião;
-    int preço;
-    int capacidade;
-    int reservas;
-    passageiro passageiro[];
-    public vôo(){
-        passageiro = new passageiro[capacidade];
-        reservas = 0;
-    }
-    public vôo(int numVoo, int tipoAvião, int preço) {
-        this.numVoo = numVoo;
-        this.tipoAvião = tipoAvião;
-        this.preço = preço;
-        setCapacidade();
-        passageiro = new passageiro[capacidade];
-        reservas = 0;
-    }
-    public int getNumVoo() {
-        return numVoo;
-    }
-    public void setNumVoo(int numVoo) {
-        this.numVoo = numVoo;
-    }
-    public int getTipoAvião() {
-        return tipoAvião;
-    }
-    public void setTipoAvião(int tipoAvião) {
-        this.tipoAvião = tipoAvião;
-        setCapacidade();
-    }
-    public int getPreço() {
-        return preço;
-    }
-    public void setPreço(int preço) {
-        this.preço = preço;
-    }
-    public passageiro getPassageiro() {
-        return passageiro[reservas];
-    }
-    public void setPassageiro(passageiro p) {
-        passageiro[reservas] = new passageiro();
-        passageiro[reservas] = p;
-    }
-    public int getReservas() {
-        return reservas;
-    }
-    public void setReservas() {
-        reservas = 0;
-    }
-    public int getCapacidade() {
-        return capacidade;
-    }
-    public void setCapacidade() {
-        if(getTipoAvião()==707) {
-            capacidade=10;
+public class Aula7 {
+    public static void main(String[] args) {
+        int numVoos=2;
+        passageiro p = new passageiro();
+        vôo voo[] = new vôo[numVoos];
+        //cria os vôos
+        for (int i=0; i<numVoos; i++) { //mudar para 5
+            voo[i] = new vôo();
+            voo[i].setNumVoo(Integer.parseInt(JOptionPane.showInputDialog("Digite o número do vôo:")));
+            voo[i].setTipoAvião(Integer.parseInt(JOptionPane.showInputDialog("Digite o tipo de avião utilizado:")));
+            voo[i].setPreço(Integer.parseInt(JOptionPane.showInputDialog("Digite o preço da passagem:")));
         }
-        else {
-            if(getTipoAvião()==727) {
-                capacidade=7;
-            }
-            else {
-                if(getTipoAvião()==737) {
-                    capacidade=2; //mudar pra 5
+        //recebe os pedidos de reserva
+        p.setId(Integer.parseInt(JOptionPane.showInputDialog("Digite a identidade:")));
+        while (p.getId()!=0) {
+            p.setVoo(Integer.parseInt(JOptionPane.showInputDialog("Digite o vôo desejado: ")));
+            //encontra o vôo desejado
+            for (int i=0; i<numVoos; i++) {
+                if (voo[i].getNumVoo()==p.getVoo()) {
+                    if(voo[i].checkCapacidade()) {
+                        voo[i].setPassageiro(p);
+                        JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() +"\nPreço da passagem: " 
+                        + voo[i].getPreço() + "\nRESERVA CONFIRMADA");
+                        voo[i].reservas();
+                    }
+                    else {
+                    JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() + "\nVÔO LOTADO");
+                    }
                 }
             }
-        }
-    }
-    public boolean checkCapacidade() {
-        return reservas<capacidade;
-    }
-    public void reservas() {
-        reservas++;
+            p.setId(Integer.parseInt(JOptionPane.showInputDialog("Digite a identidade:")));
+        }    
     }
 }
