f434db5e656dc5eaaf3739750a5c7e081d85ace1
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-03-31 22:41:16 -0300

    Update Aula7.java

--- aula7/Aula7.java
+++ aula7/Aula7.java
@@ -7,10 +7,11 @@ import javax.swing.JOptionPane;
  */
 public class Aula7 {
     public static void main(String[] args) {
+        int numVoos=2;
         passageiro p = new passageiro();
-        vôo voo[] = new vôo[2];
+        vôo voo[] = new vôo[numVoos];
         //cria os vôos
-        for (int i=0; i<1; i++) { //mudar para 5
+        for (int i=0; i<numVoos; i++) { //mudar para 5
             voo[i] = new vôo();
             voo[i].setNumVoo(Integer.parseInt(JOptionPane.showInputDialog("Digite o número do vôo:")));
             voo[i].setTipoAvião(Integer.parseInt(JOptionPane.showInputDialog("Digite o tipo de avião utilizado:")));
@@ -18,39 +19,23 @@ public class Aula7 {
         }
         //recebe os pedidos de reserva
         p.setId(Integer.parseInt(JOptionPane.showInputDialog("Digite a identidade:")));
-        p.setVoo(Integer.parseInt(JOptionPane.showInputDialog("Digite o vôo desejado: ")));
-        //encontra o vôo desejado
-        int i;
-        for(i=0; i<2 && voo[i].getNumVoo()!=p.getVoo(); i++) {}
-            if(voo[i].checkCapacidade()) {
-                voo[i].setPassageiro(p);
-                JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() +"\nPreço da passagem" 
-                + voo[i].getPreço() + "\nRESERVA CONFIRMADA");
-                voo[i].reservas();
-            }
-            else {
-                JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() + "\nVÔO LOTADO");
-            }
-        /*while (p.getId()!=0) {
-            p.setId(Integer.parseInt(JOptionPane.showInputDialog("Digite a identidade:")));
+        while (p.getId()!=0) {
             p.setVoo(Integer.parseInt(JOptionPane.showInputDialog("Digite o vôo desejado: ")));
             //encontra o vôo desejado
-            for (int i=0; i<2; i++) {
-                if (p.getVoo()==voo[i].getNumVoo()) {
-                    //checa a disponibilidade para tal vôo
-                    voo[i].disponibilidade(p);
-                    //checa se reserva foi efetuada
-                    if (p==voo[i].getPassageiro()) {
-                        JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() +"\nPreço da passagem" 
+            for (int i=0; i<numVoos; i++) {
+                if (voo[i].getNumVoo()==p.getVoo()) {
+                    if(voo[i].checkCapacidade()) {
+                        voo[i].setPassageiro(p);
+                        JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() +"\nPreço da passagem: " 
                         + voo[i].getPreço() + "\nRESERVA CONFIRMADA");
+                        voo[i].reservas();
                     }
                     else {
-                        JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() + "\nVÔO LOTADO");
+                    JOptionPane.showMessageDialog(null, "Identidade: " + p.getId() + "\nVôo: " + p.getVoo() + "\nVÔO LOTADO");
                     }
                 }
             }
-        }
-*/
+            p.setId(Integer.parseInt(JOptionPane.showInputDialog("Digite a identidade:")));
+        }    
     }
 }
-
