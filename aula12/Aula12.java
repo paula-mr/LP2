c8af0d678ddaef74870059e6a9bae77150b75c3a
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-04-25 08:43:32 -0300

    Update Aula12.java
    arrumado pra receber dados

--- aula12/Aula12.java
+++ aula12/Aula12.java
@@ -7,14 +7,14 @@ import javax.swing.JOptionPane;
  */
 public class Aula12 {
     public static void main(String[] args) {
-        double raio = 5;
-        double largura = 5;
-        double altura = 3;
-        Circulo c = new Circulo(raio);
+        Circulo c = new Circulo();
+        c.setRaio(Integer.parseInt(JOptionPane.showInputDialog("Digite o raio do círculo: ")));
         JOptionPane.showConfirmDialog(null, "Área do círculo: " + c.getArea()
-        + "\nPerímetro do círculo: " + c.getPerimetro());
-        Retangulo r = new Retangulo(largura, altura);
+        + "\nPerímetro do círculo: " + c.getPerimetro() + "\nData de criação: " + c.getData());
+        Retangulo r = new Retangulo();
+        r.setAltura(Integer.parseInt(JOptionPane.showInputDialog("Digite a altura do retângulo: ")));
+        r.setLargura(Integer.parseInt(JOptionPane.showInputDialog("Digite a largura do retângulo: ")));
         JOptionPane.showConfirmDialog(null, "Área do retângulo: " + r.getArea()
-        + "\nPerímetro do retângulo: " + r.getPerimetro());
+        + "\nPerímetro do retângulo: " + r.getPerimetro() + "\nData de criação: " + r.getData());
     }
 }
